82dcc8b6a77e63b4da9a7e60ff50e5c7e3fdac84
htdesign <h.webdesigns@gmail.com>
2017-07-19 14:12:05 +1200

    Fixed Sticky Navigation

--- js/page.js
+++ js/page.js
@@ -1,3 +1,11 @@
 $('.js-tilt').tilt({
     scale: 1.2
 })
+
+// let animatedBounce = 'animated bounce';
+
+// $('h1').hover(function(){
+//     $(this).addClass(animatedBounce);
+// }).mouseout(function(){
+//     $(this).removeClass(animatedBounce);
+// });
--- style/config.rb
+++ style/config.rb
@@ -1,9 +1,9 @@
 http_path = "/"
         css_dir = "."
         sass_dir = "."
-        images_dir = "img"
-        javascripts_dir = "javascripts"
-        output_style = :nested
+        images_dir = "images"
+        javascripts_dir = "js"
+        output_style = :expanded
         relative_assets=true
         line_comments = false
         
