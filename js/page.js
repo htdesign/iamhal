34b9f0263ac6d959ff00e0619b5bc11fda5213f8
htdesign <h.webdesigns@gmail.com>
2017-08-05 01:42:35 +1200

    applied new design

--- js/page.js
+++ js/page.js
@@ -10,3 +10,20 @@ $.stellar({
     positionProperty: 'transform',
     hideDistantElements: false
 });
+
+
+let hotel = {
+    name: 'Park',
+    rooms: 120,
+    booked: 77,
+    checkAvailability: function () {
+        return this.rooms - this.booked;
+    }
+}
+
+
+var today = new Date();
+var year = today.getFullYear();
+
+var el = document.getElementById('footer');
+el.innerHTML = '<p>Copyright &copy;' + year + '</p>';
